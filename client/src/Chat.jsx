4f8f47ba6517b991e0344c30fb018d4d9642bf92
Zeztron <pateltharsh@gmail.com>
2021-07-23 08:56:20 -0500

    style chat messages

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -6,6 +6,7 @@ import {
   useQuery,
   gql,
 } from '@apollo/client';
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -27,14 +28,55 @@ const Messages = ({ user }) => {
   console.log(data);
   if (!data) return null;
 
-  return JSON.stringify(data);
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          key={id}
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: '0.5em',
+                border: '2px solid',
+                borderRadius: 25,
+                textAlign: 'center',
+                fontSize: '18pt',
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? '#58bf56' : '#e5e6ea',
+              color: user === messageUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 const Chat = () => {
   return (
-    <div>
-      <Messages user="Jack" />
-    </div>
+    <Container>
+      <Messages user="haha" />
+    </Container>
   );
 };
 
